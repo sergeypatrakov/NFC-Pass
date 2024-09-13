9d4d9d402634c694f5862820f2c932dae36979e3
Sergey <patrakovsergo@gmail.com>
2024-09-13 14:07:48 +0300

    start project, choose UIKit & delete Storyboard, create NavigationController

---
+++ NFC Pass/NFC Pass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NFC Pass
+//
+//  Created by Sergey Patrakov on 13.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
